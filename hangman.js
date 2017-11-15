bd15f674c83b59c5b28c45e0f975e6a5d68fbb79
philip dierks <phillipdierks@students.berkeley.net>
2017-11-15 11:40:19 -0800

    asa

--- hangman.js
+++ hangman.js
@@ -2,30 +2,29 @@ function setUpVars() {
     guesses = 10;
     word = "";
     guessedletters = [];
-    words = ["bayern", "dortmund", "chelsea","arsenal","mbappe"];
+    words = ["bayern", "dortmund", "chelsea","arsenal"];
     guess = document.getElementById("guess").value;
-    document.getElementById("underscore").innerHTML = "_";
+
 }
 
 
 function startGame(){
-    var num = "";
+    document.getElementById("underscore").innerHTML = "";
     word = words[Math.floor(Math.random() * words.length)];
     for(var i = 0; i < word.length; i++){
-        num+= "_"
+        document.getElementById("underscore").innerHTML += "<div>_ </div>";
     }
-    printWord();
 }
 
 function printWord(){
-var r = "";
+ var r = "";
 for(var i = 0; i < word.length; i++){
     if(guessedletters.indexOf(word[i]) > -1){
         r += word[i];
     }else{
         r += "_";
     }
-    document.getElementById("underscore").innerHTML = printWord();
+    document.getElementById("guessLetter").innerHTML = printWord();
 }
 }
 function handleGuess(){
