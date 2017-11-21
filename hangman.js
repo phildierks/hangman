dd16ab8c696d6c17c30be35755d85c58f5fbdb33
philip dierks <phillipdierks@students.berkeley.net>
2017-11-21 11:36:28 -0800

    asa

--- hangman.js
+++ hangman.js
@@ -13,16 +13,18 @@ function setUpVars() {
 
 function clearFields(){
     document.getElementById("guess").value = "";
-
+    document.getElementById("wrongLetter").value = "";
 }
 
 function enableGuess(){
     document.getElementById("guessLetter").disabled = false;
 }
 
+function clearGuesses(){
+    document.getElementById("guessesLeft").innerHTML = "";
+}
 
 function startGame(){
-    document.getElementById("hangmanPic").innerHTML ="<img src='img/hangman7.png'/>";
     guessesLeft = 6;
     document.getElementById("wrongLetter").innerHTML="";
     document.getElementById("win?").innerHTML="";
@@ -75,7 +77,7 @@ function printWord() {
         }
         document.getElementById("displayedWord").innerHTML = displayedGuess ;
     guesses();
-    document.getElementById("guessedLetters").innerHTML  += guess + ", ";
+    document.getElementById("guessedLetters").innerHTML  += guess + " ";
     if(word == displayedGuess){
         document.getElementById("win?").innerHTML = "You win! The word was:" + word +  ". Press Start Game to play again!";
         document.getElementById("category").disabled = false;
@@ -87,40 +89,27 @@ function printWord() {
 
 
 
-function guesses(){
-    if((word.indexOf(guess) == -1)){
+function guesses() {
+    if ((word.indexOf(guess) == -1)) {
         guessesLeft--;
         document.getElementById("guessesLeft").innerHTML = "Guesses Left: " + guessesLeft;
     }
+    var inLetterList = false;
+    for (var i = 0; i < guessedletters.length; i++) {
+        if (guess == guessedletters[i]) {
+            inLetterList = true;
+        }
+    }
+    if (!inLetterList) {
+        document.getElementById("wrongLetter").innerHTML = "You already guessed that.";
+    }
 
-    if(guessesLeft==0){
-        document.getElementById("win?").innerHTML= "You Lose! The word was " + word + ". Press Start Game to play again!";
+
+    if (guessesLeft == 0) {
+        document.getElementById("win?").innerHTML = "You Lose! The word was " + word + ". Press Start Game to play again!";
         document.getElementById("guessLetter").disabled = true;
         document.getElementById("category").disabled = false;
         document.getElementById("startGame").disabled = false;
     }
+}
 
-}
-function images(){
-if(guessesLeft == 6){
-    return    "<img src=img/hangman7.png>";
-}
-if(guessesLeft == 5){
-    return    "<img src=img/hangman6.png>";
-}
-if(guessesLeft == 4){
-    return    "<img src=img/hangman5.png>";
-}
-if(guessesLeft == 3){
-    return    "<img src=img/hangman4.png>";
-}
-if(guessesLeft == 2){
-    return    "<img src=img/hangman3.png>";
-}
-if(guessesLeft == 1){
-    return    "<img src=img/hangman2.png>";
-}
-if(guessesLeft == 0){
-    return    "<img src=img/hangman1.png>";
-}
-}
