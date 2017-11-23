3cf648afc40047f2388b255740dfbcecacf50400
phildierks <phillipdierks@students.berkeley.net>
2017-11-22 19:32:03 -0800

    at home work

--- hangman.js
+++ hangman.js
@@ -1,34 +1,37 @@
 function setUpVars() {
+    document.getElementById("guessedLetter").innerHTML = "";
     word = "";
     guessedletters = [];
-    soccerTeams = ["bayern", "dortmund", "chelsea","arsenal"];
-    animals = ["duck","goose","rat","mouse","dog","elephant","rooster","monkey","penguin"];
-    schoolSupplies = ["pencil","paper","eraser","divider","binder","calculator","highlighter"];
+    soccerTeams = ["bayern", "dortmund", "chelsea","arsenal","psg","roma","juventus","tottenham","liverpool","schalke","barcelona","celtic","anderlecht","ajax"];
+    animals = ["duck","goose","rat","mouse","dog","elephant","rooster","monkey","penguin","gecko","deer","snake","clownfish","cat","lion","tiger","panther"];
+    schoolSupplies = ["pencil","paper","eraser","divider","binder","calculator","highlighter","backpack","book","ruler","compass","marker","pen","crayon","scissors","lunchbox","sharpener"];
     guess = document.getElementById("guess").value;
     document.getElementById("guessLetter").disabled = true;
-    possibleLetters = ["a","b","c","d","e","f","g","h","i","j","k","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+    possibleLetters = ["0","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 }
 
 
-
-function clearFields(){
-    document.getElementById("guess").value = "";
-    document.getElementById("wrongLetter").value = "";
-}
-
-function enableGuess(){
-    document.getElementById("guessLetter").disabled = false;
-}
-
-function clearGuesses(){
-    document.getElementById("guessesLeft").innerHTML = "";
-}
 
 function startGame(){
-    guessesLeft = 6;
-    document.getElementById("wrongLetter").innerHTML="";
-    document.getElementById("win?").innerHTML="";
+    document.getElementById("guessesLeft").innerHTML = "Guesses Left: 7";
+    document.getElementById("guessedLetter").innerHTML= "Guessed Letters: ";
+    var box = document.getElementById("guess");
+    for(var a=0;a<26;a++){
+    if(guessedletters.indexOf(possibleLetters[i])==-1){
+        var guessed = document.getElementById("guess").selectedIndex;
+        box.remove(guessed);
+    }
+}
+
+    guessedletters = [];
+
+    guessesLeft = 7;
+    document.getElementById("win").innerHTML="";
     document.getElementById("displayedWord").innerHTML = "";
+    for(var i=1; i<=26; i++){
+        var possibleguess = possibleLetters[i];
+            document.getElementById("guess").innerHTML += "<option value=" + possibleguess + ">" + possibleguess + "</option>";
+        }
     var category = document.getElementById("category").value;
     var cat = parseInt(category);
 if(cat == 1) {
@@ -48,24 +51,15 @@ if(cat == 3){
     document.getElementById("startGame").disabled = true;
     document.getElementById("displayedWord").innerHTML = displayedLetters;
     console.log(word);
+    document.getElementById("guessLetter").disabled = false;
  }
 
 
 
 function printWord() {
-    guess = document.getElementById("guess").value;
-    guess = guess.toLowerCase();
-    guessedletters.push(guess);
-    var inLetterList = false;
-    for(var i = 0; i < possibleLetters.length; i++){
-        if(guess == possibleLetters[i]){
-            inLetterList = true;
-        }
-    }
-    if(!inLetterList) {
-        document.getElementById("wrongLetter").innerHTML = "You can only enter letters.";
-    }
 
+    guess = document.getElementById("guess").value;
+    guessedletters.push(guess);
         var displayedGuess = "";
         for (var i = 0; i < word.length; i++) {
             if (guessedletters.indexOf(word[i]) > -1) {
@@ -75,38 +69,38 @@ function printWord() {
                 displayedGuess += "_ ";
             }
         }
+
+
         document.getElementById("displayedWord").innerHTML = displayedGuess ;
     guesses();
-    document.getElementById("guessedLetters").innerHTML  += guess + " ";
+    document.getElementById("guessedLetter").innerHTML+= guess + " ";
+    removeOption(guess);
+    document.getElementById("displayedWord").innerHTML= displayedGuess;
+
     if(word == displayedGuess){
-        document.getElementById("win?").innerHTML = "You win! The word was:" + word +  ". Press Start Game to play again!";
+        document.getElementById("win").innerHTML = "You win! The word was: " + word +  ". Press Start Game to play again!";
         document.getElementById("category").disabled = false;
         document.getElementById("startGame").disabled = false;
+        document.getElementById("guessLetter").disabled = true;
     }
+}
+
+function removeOption(){
+    var guessed = document.getElementById("guess").selectedIndex;
+    var select = document.getElementById("guess");
+    select.remove(guessed);
 }
 
 
 
-
-
-function guesses() {
+function guesses(){
     if ((word.indexOf(guess) == -1)) {
         guessesLeft--;
         document.getElementById("guessesLeft").innerHTML = "Guesses Left: " + guessesLeft;
     }
-    var inLetterList = false;
-    for (var i = 0; i < guessedletters.length; i++) {
-        if (guess == guessedletters[i]) {
-            inLetterList = true;
-        }
-    }
-    if (!inLetterList) {
-        document.getElementById("wrongLetter").innerHTML = "You already guessed that.";
-    }
 
-
-    if (guessesLeft == 0) {
-        document.getElementById("win?").innerHTML = "You Lose! The word was " + word + ". Press Start Game to play again!";
+    if(guessesLeft == 0){
+        document.getElementById("win").innerHTML = "You Lose! The word was " + word + ". Press Start Game to play again!";
         document.getElementById("guessLetter").disabled = true;
         document.getElementById("category").disabled = false;
         document.getElementById("startGame").disabled = false;
